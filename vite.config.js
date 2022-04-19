09838b91c748fec1a49f2ce111a46b46e9949ff2
Thiago William <thiagokepler1@gmail.com>
2022-04-19 19:11:34 -0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cronometro-vue/",
   plugins: [vue()],
-  publicPath: "/cronometro-vue/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
